5c522bab51061ec3b3e0cb3dfee55d4e76b1c3f4
Gabriel P. Santos <gabrielp_santos@hotmail.com>
2023-11-04 19:54:18 -0300

    first commit

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Esse arquivo permite importar imagens existentes no projeto react como 'modulos',
+ * o que facilita seu uso com a tag <Image> do Next.js
+ */
+
+declare module "*.jfif"
+declare module "*.jpg";
+declare module "*.png";
