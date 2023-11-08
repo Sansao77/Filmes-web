c305db6497932304502f3d6e057fcc8a58b2f39b
sthallysson <sthallysson5@gmail.com>
2023-11-07 22:03:07 -0300

    style: add responsive && refactor: ajusted folders structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 
-import Header from "./components/Header";
-import Menu from "./components/Menu";
-import { Introducao } from "./components/Header";
-import MovieList from "./components/MovieList";
+import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu";
+import { Introducao } from "./components/Header/Header";
+import MovieList from "./components/MovieList/MovieList";
 
 export default function Home() {
   return (
